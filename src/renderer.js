ee6df46282c90a7e532983926ed198738777c6bc
vsvipul <vsvipul555@gmail.com>
2019-02-23 02:28:56 +0530

    add mouseup event

--- src/renderer.js
+++ src/renderer.js
@@ -6,20 +6,25 @@ document.getElementById('myButton').addEventListener('click', () => {
     properties: ['openFile'], 
     filters: [ { name: "PDFs", extensions: ['pdf'] } ] 
   }, (filepaths) => {
-    const filePath = filepaths[0];
-    const viewerEle = document.getElementById('viewer');
-    viewerEle.innerHTML = ''; 
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute("id", "pdf-iframe");
-    iframe.setAttribute("onLoad", "addPDFListener()");
-    iframe.src = path.resolve(__dirname, `../public/pdfjs/web/viewer.html?file=${filePath}`);
-    viewerEle.appendChild(iframe);
-    iframe.onload = function() {
-      var ifr =document.getElementById('pdf-iframe');
-      ifr.contentDocument.body.addEventListener('contextmenu', function(){
-        var idoc= ifr.contentDocument || ifr.contentWindow.document; // ie compatibility
-        console.log(idoc.getSelection().toString());
-      });
-    }
+    loadPDF(filepaths);
   });
 });
+
+var loadPDF = (filepaths) => {
+  const filePath = filepaths[0];
+  const viewerEle = document.getElementById('viewer');
+  viewerEle.innerHTML = ''; 
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute("id", "pdf-iframe");
+  iframe.setAttribute("onLoad", "addPDFListener()");
+  iframe.src = path.resolve(__dirname, `../public/pdfjs/web/viewer.html?file=${filePath}`);
+  viewerEle.appendChild(iframe);
+  iframe.onload = function(){
+    console.log('avx');
+    var ifr =document.getElementById('pdf-iframe');
+    ifr.contentDocument.body.addEventListener('mouseup', function(){
+      var idoc= ifr.contentDocument || ifr.contentWindow.document; // ie compatibility
+      console.log(idoc.getSelection().toString());
+    });
+  }
+}
